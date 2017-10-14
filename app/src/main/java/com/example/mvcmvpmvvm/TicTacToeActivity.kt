b3d78ab7f0f5d813c4099ffbd515e9863ab25476
Peng Ye <pengcy783@gmail.com>
2017-10-14 11:08:40 -0500

    update

--- app/src/main/java/com/example/mvcmvpmvvm/TicTacToeActivity.kt
+++ app/src/main/java/com/example/mvcmvpmvvm/TicTacToeActivity.kt
@@ -3,10 +3,11 @@ package com.example.mvcmvpmvvm
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+
+class TicTacToeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.tictactoe)
     }
 }
